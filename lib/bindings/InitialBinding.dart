61d2e7c11b888ef959e5013efbec84138ffc6477
vikasuniyal1m <vikasuniyal1m@gmail.com>
2026-01-27 15:34:03 +0530

    added the initial binding for all controller to make prevent the lazy loading issue

--- lib/bindings/InitialBinding.dart
+++ lib/bindings/InitialBinding.dart
@@ -11,23 +11,77 @@ import 'package:fruitsofspirit/controllers/notifications_controller.dart';
 import 'package:fruitsofspirit/controllers/main_dashboard_controller.dart';
 import 'package:fruitsofspirit/services/jingle_service.dart';
 
+// New Imports
+import 'package:fruitsofspirit/controllers/group_chat_controller.dart';
+import 'package:fruitsofspirit/controllers/group_posts_controller.dart';
+import 'package:fruitsofspirit/controllers/prayer_reminders_controller.dart';
+import 'package:fruitsofspirit/controllers/onboarding_controller.dart';
+import 'package:fruitsofspirit/controllers/phone_auth_controller.dart';
+import 'package:fruitsofspirit/controllers/forgot_password_controller.dart';
+
 class InitialBinding extends Bindings {
   @override
   void dependencies() {
     // Making ALL core controllers permanent to prevent "not found" errors
     // throughout the app lifecycle.
-    Get.put(HomeController(), permanent: true);
-    Get.put(PrayersController(), permanent: true);
-    Get.put(GroupsController(), permanent: true);
-    Get.put(NotificationsController(), permanent: true);
-    Get.put(ProfileController(), permanent: true);
-    Get.put(FruitsController(), permanent: true);
-    Get.put(BlogsController(), permanent: true);
-    Get.put(VideosController(), permanent: true);
-    Get.put(GalleryController(), permanent: true);
-    Get.put(MainDashboardController(), permanent: true);
+
+    // 1. Core Controllers
+    if (!Get.isRegistered<HomeController>()) {
+      Get.put(HomeController(), permanent: true);
+    }
+    if (!Get.isRegistered<MainDashboardController>()) {
+      Get.put(MainDashboardController(), permanent: true);
+    }
+    if (!Get.isRegistered<NotificationsController>()) {
+      Get.put(NotificationsController(), permanent: true);
+    }
+    if (!Get.isRegistered<ProfileController>()) {
+      Get.put(ProfileController(), permanent: true);
+    }
+
+    // 2. Feature Controllers
+    if (!Get.isRegistered<PrayersController>()) {
+      Get.put(PrayersController(), permanent: true);
+    }
+    if (!Get.isRegistered<PrayerRemindersController>()) {
+      Get.put(PrayerRemindersController(), permanent: true);
+    }
+    if (!Get.isRegistered<GroupsController>()) {
+      Get.put(GroupsController(), permanent: true);
+    }
+    if (!Get.isRegistered<GroupChatController>()) {
+      Get.put(GroupChatController(), permanent: true);
+    }
+    if (!Get.isRegistered<GroupPostsController>()) {
+      Get.put(GroupPostsController(), permanent: true);
+    }
+    if (!Get.isRegistered<FruitsController>()) {
+      Get.put(FruitsController(), permanent: true);
+    }
+    if (!Get.isRegistered<BlogsController>()) {
+      Get.put(BlogsController(), permanent: true);
+    }
+    if (!Get.isRegistered<VideosController>()) {
+      Get.put(VideosController(), permanent: true);
+    }
+    if (!Get.isRegistered<GalleryController>()) {
+      Get.put(GalleryController(), permanent: true);
+    }
+
+    // 3. Auth & Onboarding Controllers
+    if (!Get.isRegistered<OnboardingController>()) {
+      Get.put(OnboardingController(), permanent: true);
+    }
+    if (!Get.isRegistered<PhoneAuthController>()) {
+      Get.put(PhoneAuthController(), permanent: true);
+    }
+    if (!Get.isRegistered<ForgotPasswordController>()) {
+      Get.put(ForgotPasswordController(), permanent: true);
+    }
 
     // Voice Over Service
-    Get.put(JingleService(), permanent: true);
+    if (!Get.isRegistered<JingleService>()) {
+      Get.put(JingleService(), permanent: true);
+    }
   }
 }
